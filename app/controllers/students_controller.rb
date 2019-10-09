b616d1fb0b48386a48fb34963ca1bbf3f331cf58
Melissa Williams <melissa@brandmeetscopy.com>
2019-10-09 10:23:49 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,10 +21,13 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.search(params[:query])
+    # @student = Student.find_by(params[:query])
   end
 
   def student_params
-    params.require(:student).permit(:name, :birthday, :hometown)
+    params.require(:student).permit(:name, :birthday, :hometown, :query)
   end
+
+  
 end
